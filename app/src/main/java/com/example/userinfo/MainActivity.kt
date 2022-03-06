df9e5585906d1ec240d6682fa69fc768e18d5454
shireen <syed.shireen@everest.engineering>
2022-03-06 10:01:40 +0530

    view groups added.

--- app/src/main/java/com/example/userinfo/MainActivity.kt
+++ app/src/main/java/com/example/userinfo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.userinfo
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -10,41 +11,57 @@ import com.example.userinfo.databinding.ActivityMainBinding
 
 class MainActivity() : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var visibleFieldsCheck: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-         val userName = binding.userNameET.text.toString().trim()
-        val email = binding.emailET.text.toString().trim()
-        val phoneNumber = binding.phoneNumberET.text.toString().trim()
-        val pinCode = binding.pinCodeET.text.toString().trim()
-        val address = binding.addressET.text.toString().trim()
+        binding.textGroup.visibility = View.INVISIBLE
+        binding.editGroup.visibility = View.VISIBLE
         binding.validate.setOnClickListener {
-            if (fieldsValidationToCheckAllFieldsAreEntered(userName,email,phoneNumber,pinCode,address) && emailValidation(email) && phoneNumberValidation(phoneNumber) && pinCodeValidation(pinCode)) {
+            val userName = binding.userNameET.text.toString().trim()
+            val email = binding.emailET.text.toString().trim()
+            val phone = binding.phoneNumberET.text.toString().trim()
+            val pinCode = binding.pinCodeET.text.toString().trim()
+            val address = binding.addressET.text.toString().trim()
+            binding.textGroup.visibility = View.VISIBLE
+            binding.editGroup.visibility = View.INVISIBLE
+
+            if (fieldsValidationToCheckAllFieldsAreEntered(
+                    userName,
+                    email,
+                    phone,
+                    pinCode,
+                    address
+                )
+            ) {
                 binding.validUserName.setText(userName)
                 binding.validEmail.setText(email)
-                binding.validPhoneNumber.setText(phoneNumber)
+                binding.validPhoneNumber.setText(phone)
                 binding.validPinCode.setText(pinCode)
                 binding.validAddress.setText(address)
-                visibleFieldsCheck = true
-                fieldsDisplayOnValidate(visibleFieldsCheck)
+                binding.textGroup.visibility = View.VISIBLE
+                binding.editGroup.visibility = View.INVISIBLE
                 binding.confirm.setOnClickListener {
 
                     dataPassingFromActivityToActivityOnConfirm()
                 }
                 binding.cancel.setOnClickListener {
-                    visibleFieldsCheck = false
-                    fieldsDisplayOnValidate(visibleFieldsCheck)
+                    binding.editGroup.visibility = View.VISIBLE
+                    binding.textGroup.visibility = View.INVISIBLE
+
                 }
-
             }
-
 
         }
     }
 
-   fun fieldsValidationToCheckAllFieldsAreEntered(userName:String,email: String,phone: String,pinCode: String,address:String): Boolean {
+    fun fieldsValidationToCheckAllFieldsAreEntered(
+        userName: String,
+        email: String,
+        phone: String,
+        pinCode: String,
+        address: String
+    ): Boolean {
         if ((userName.isEmpty()) || (email.isEmpty()) || (phone.isEmpty()) || (pinCode.isEmpty()) || (address.isEmpty())) {
             Toast.makeText(this, "Every field is mandatory", Toast.LENGTH_LONG).show()
             return false
@@ -54,7 +71,7 @@ class MainActivity() : AppCompatActivity() {
         }
     }
 
-     fun phoneNumberValidation(phone:String): Boolean {
+    fun phoneNumberValidation(phone: String): Boolean {
         if (phone.length == 10) {
             return true
         } else {
@@ -64,12 +81,12 @@ class MainActivity() : AppCompatActivity() {
         }
     }
 
-     fun pinCodeValidation(pinCode:String): Boolean {
+    fun pinCodeValidation(pinCode: String): Boolean {
         if (pinCode.length == 6) {
             return true
         } else {
-            Toast.makeText(this, "Pin code ( should be only length of 6) ", Toast.LENGTH_LONG)
-                .show()
+            val toastMessages = ToastMessages()
+            toastMessages.showToast(applicationContext,"Pin code should be of length 6")
             return false
         }
     }
@@ -91,24 +108,32 @@ class MainActivity() : AppCompatActivity() {
         outState.putString(PHONENUMBER, binding.validPhoneNumber.text.toString())
         outState.putString(PINCODE, binding.validPinCode.text.toString())
         outState.putString(ADDRESS, binding.validAddress.text.toString())
-        outState.putBoolean(VISIBLE, visibleFieldsCheck)
+        outState.putBoolean(VISIBLE, binding.editGroup.isVisible)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        if (savedInstanceState.getBoolean(VISIBLE)) {
+
+        println(savedInstanceState.getBoolean(VISIBLE))
+        if(savedInstanceState.getBoolean(VISIBLE)) {
+
+            binding.textGroup.visibility = View.INVISIBLE
+            binding.editGroup.visibility = View.VISIBLE
+        }
+        else{
+            binding.textGroup.visibility = View.VISIBLE
+            binding.editGroup.visibility = View.INVISIBLE
             binding.validUserName.setText(savedInstanceState.getString(USERNAME))
             binding.validEmail.setText(savedInstanceState.getString(EMAIL))
             binding.validPhoneNumber.setText(savedInstanceState.getString(PHONENUMBER))
             binding.validPinCode.setText(savedInstanceState.getString(PINCODE))
             binding.validAddress.setText(savedInstanceState.getString(ADDRESS))
-            fieldsDisplayOnValidate(savedInstanceState.getBoolean(VISIBLE))
-            visibleFieldsCheck = true
             binding.confirm.setOnClickListener {
                 dataPassingFromActivityToActivityOnConfirm()
             }
             binding.cancel.setOnClickListener {
-                fieldsDisplayOnValidate(false)
-                visibleFieldsCheck = false
+
+                binding.textGroup.visibility = View.INVISIBLE
+                binding.editGroup.visibility = View.VISIBLE
             }
 
         }
@@ -116,22 +141,6 @@ class MainActivity() : AppCompatActivity() {
 
     }
 
-    private fun fieldsDisplayOnValidate(visible: Boolean) {
-        binding.validUserName.isVisible = visible
-        binding.validEmail.isVisible = visible
-        binding.validPhoneNumber.isVisible = visible
-        binding.validPinCode.isVisible = visible
-        binding.validAddress.isVisible = visible
-        binding.confirm.isVisible = visible
-        binding.cancel.isVisible = visible
-        binding.userNameET.isVisible = !visible
-        binding.emailET.isVisible = !visible
-        binding.phoneNumberET.isVisible = !visible
-        binding.pinCodeET.isVisible = !visible
-        binding.addressET.isVisible = !visible
-        binding.validate.isVisible = !visible
-
-    }
 
     private fun dataPassingFromActivityToActivityOnConfirm() {
         val intent = Intent(this, DetailsActivity::class.java)
